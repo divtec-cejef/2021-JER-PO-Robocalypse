c208e52aaf5164c34de434f623cb44c4c04df610
Sarah Aubry <sar23621@gmail.com>
2021-06-24 15:43:46 +0200

    L'UFO lévite et se déplace aléatoirement
    revoir déplacements au lieu de faire des tp

--- robocalypse-3d/Assets/Imports/Script/UFO.cs
+++ robocalypse-3d/Assets/Imports/Script/UFO.cs
@@ -10,45 +10,84 @@ public class UFO : MonoBehaviour
 {
     public GameObject player;
 
-    private Vector3 position;
 
-    private Vector3[] positionsPossibles = {(new Vector3(-0.3f, 0.8f, 0.1f)), (new Vector3(0.8f,1.5f,0.1f))};
+    private Vector3 choix;
+    private Vector3 ancien;
+
+    // Où l'UFO se déplacera la prochaine fois
+    // private Vector3 futurePosition;
+    Vector3[] positionsPossibles = {(new Vector3(-0.8f, 0.9f, 0.2f)),
+        (new Vector3(0.8f,1.2f,0.2f)),
+        (new Vector3(1f,0.4f,0.2f)),
+        (new Vector3(-1f,0.2f,0.2f))};
+
+    private Vector3 position;
     
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(changePosition());
-        // InvokeRepeating("changePosition", 2f, 2f);
+        choix = positionsPossibles[0];
+        // UFOposition();
+        // StartCoroutine(wait());
+        InvokeRepeating(nameof(UFOposition), .1f, 2f);
+        
+        
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        // lévitation
+        Vector3 posOffset = new Vector3 ();
+        Vector3 tempPos = new Vector3 ();
+        tempPos = posOffset;
+        tempPos.y += Mathf.Sin (Time.fixedTime * Mathf.PI * 3f) * 0.1f; // fréquence, amplitude
+        
         position = player.transform.position;
-        transform.position = position + positionsPossibles[0];
+ 
+        transform.position = tempPos;
         
+        transform.position = position + choix + tempPos; // + choix au lieu de tempPos
         
     }
 
-    /**
-     * L'UFO change de position toutes les deux secondes.
-     */
-    IEnumerator changePosition()
+    void UFOposition()
     {
-        while (true)
+        int rand = Random.Range(0, positionsPossibles.Length);
+        
+        choix = positionsPossibles[rand];
+        if (choix == ancien)
         {
-            print("Debut");
-        
-            int rand = Random.Range(0, positionsPossibles.Length);
-        
-            transform.position = position + positionsPossibles[rand];
-        
-            yield return new WaitForSeconds(2f);
-            print("FIN");
+            choix = positionsPossibles[rand];
         }
-        
 
-        // yield return new WaitForSeconds(2);
+        ancien = choix;
+        Debug.Log("position changée");
+        
     }
+
+   
+    /*IEnumerator wait()
+    {
+            // position = player.transform.position;
+            int rand = Random.Range(0, positionsPossibles.Length);
+
+            yield return new WaitForSeconds(2f);
+
+            Debug.ClearDeveloperConsole();
+            transform.position = position + positionsPossibles[rand];
+
+            // transform.position = position + positionsPossibles[1];
+
+            yield return new WaitForSeconds(2f);
+
+            Debug.Log("OK");
+        
+        
+        // transform.position = position + positionsPossibles[1];
+        
+        // yield return new WaitForSeconds(2);
+    }*/
 
 }
